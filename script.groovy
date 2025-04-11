589ac2c03a714278f84ca0983e96863db2ca04fa
AMR HABIB <amrhabib123451@gmail.com>
2025-04-11 22:58:17 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,22 +1,17 @@
 def buildApp() {
     echo "🔧 Building the automation project with Maven..."
-    // Clean and compile the project
-    sh 'mvn clean compile'
+    bat 'mvn clean compile'
 }
 
 def testApp() {
     echo "🧪 Running TestNG tests..."
-    // Run tests and generate reports
-    sh 'mvn test'
+    bat 'mvn test'
 }
 
 def deployApp() {
     echo "📦 Archiving reports and logs..."
 
-    // Publish TestNG results (Surefire plugin)
     junit '**/target/surefire-reports/*.xml'
-
-    // Archive other artifacts (like logs, screenshots)
     archiveArtifacts artifacts: '**/target/**/*.log, **/target/screenshots/**', fingerprint: true
 }
 
